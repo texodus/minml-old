9fc8ed846c58911be8fd48bc19ea908e91051c0d
Andrew Stein <slink@slink.home>
2013-05-27 14:53:23 -0400

    Fixed test names

--- src/hs/test/IntegrationSpec.hs
+++ src/hs/test/IntegrationSpec.hs
@@ -22,26 +22,26 @@ spec = do
 
     	describe "parseForml" $ do
 
-            it "should parse a trivial example " $ assertNode
+            it "should compile & run a trivial example " $ assertNode
               
                 "   2 + 2.0 == 4.0   "
 
                 (Right "true\n")
 
-            it "should parse simple type errors" $ assertNode
+            it "should compile & run simple type errors" $ assertNode
 
                 "   4 + \"whoops\"   "
 
                 (Left (Err "Types do not unify\n  TypeSym (TypeSymT Star \"Double\") (Star) and TypeSym (TypeSymT Star \"String\") (Star)"))
 
-            it "should parse let expressions" $ assertNode
+            it "should compile & run let expressions" $ assertNode
 
                 "   let x = 1.0;     \
                 \   x + 1.0 == 2.0   "
 
                 (Right "true\n")
 
-            it "should error when trying to parse a missing let continuation" $ assertNode
+            it "should error when trying to compile & run a missing let continuation" $ assertNode
 
                 "   let x = 1;   "
 
@@ -49,20 +49,20 @@ spec = do
                     \unexpected end of input\n\
                     \expecting Let Expression, Type Kind Expression, Abstraction, Match Expression or Application"))
 
-            it "should parse anonymous functions and application" $ assertNode
+            it "should compile & run anonymous functions and application" $ assertNode
 
                 "   (fun x -> x + 4) 2 == 6   "
 
                 (Right "true\n")
 
-            it "should parse anonymous functions in let bindings" $ assertNode
+            it "should compile & run anonymous functions in let bindings" $ assertNode
              
                 "   let g = fun x -> x + 4;   \
                 \   g 2 == 6                  "
 
                 (Right "true\n")
 
-            it "should parse match expressions" $ assertNode
+            it "should compile & run match expressions" $ assertNode
 
                 "   let fib = fun n ->                      \
                 \       match n with                        \
@@ -73,7 +73,7 @@ spec = do
 
                 (Right "13\n")
 
-            it "should fail to parse match mixed semicolon" $ assertNode
+            it "should fail to compile & run match mixed semicolon" $ assertNode
 
                 "   let fib = fun n ->                      \n\
                 \       match n with                        \n\
@@ -84,13 +84,13 @@ spec = do
 
                 (Right "13\n")
 
-            it "should parse match expressions with type errors" $ assertNode
+            it "should compile & run match expressions with type errors" $ assertNode
 
                 "   match 3 + 3 with \"gotcha\" -> false;   "
 
                 (Left (Err "Types do not unify\n  TypeSym (TypeSymT Star \"String\") (Star) and TypeSym (TypeSymT Star \"Double\") (Star)"))
 
-            it "should parse user data types" $ assertNode
+            it "should compile & run user data types" $ assertNode
 
                 "   data Cons: a -> List a -> List a;        \
                 \   data Nil: List a;                        \
@@ -104,7 +104,7 @@ spec = do
 
                 (Right "2\n")
 
-            it "should parse with generic type errors" $ assertNode
+            it "should compile & run with generic type errors" $ assertNode
 
                 "   data Just: a -> Maybe a;          \
                 \   data None: Maybe a;               \
@@ -114,7 +114,7 @@ spec = do
 
                 (Left (Err "Types do not unify\n  TypeSym (TypeSymT Star \"String\") (Star) and TypeSym (TypeSymT Star \"Double\") (Star)"))
 
-            it "should parse partial keywords" $ assertNode
+            it "should compile & run partial keywords" $ assertNode
 
                 "   let letly = 4;                    \
                 \   (let func = fun x -> x + ((2));   \
@@ -122,7 +122,7 @@ spec = do
 
                 (Right "6\n")
 
-            it "should parse binding tricks" $ assertNode
+            it "should compile & run binding tricks" $ assertNode
 
                 "   let x = 3;                 \
                 \   let f = fun y -> y + x;    \
@@ -131,16 +131,25 @@ spec = do
 
                 (Right "true\n")
 
+            it "should compile & run basic unboxing" $ assertNode
+
+                "   data Just: a -> Maybe a;          \
+                \                                     \
+                \   match Just 5 with                 \
+                \       (Just x) -> x                 "
+
+                (Right "5\n")
+
             describe "function application whitespace" $ do
 
-                it "should fail to parse function application on separate lines" $ assertNode
+                it "should fail to compile & run function application on separate lines" $ assertNode
 
                     "   parseInt  \n\
                     \   4         "
 
                     $ Left (Err "\"Parsing Forml\" (line 2, column 4):\nunexpected '4'\nexpecting operator or end of input\nStatement indented (introduced at \"Parsing Forml\" (line 1, column 4))")
 
-                it "should parse function application on separate lines indented" $ assertNode
+                it "should compile & run function application on separate lines indented" $ assertNode
 
                     "   parseInt  \n\
                     \       4       "
